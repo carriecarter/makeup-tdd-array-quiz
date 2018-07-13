98cc4578af3c67c18a75eb2c8eff2f2aecfd6b26
carriecarter <carrie@carrie-carter.com>
2018-07-13 10:58:56 -0700

    pending tests

--- test/why.test.js
+++ test/why.test.js
@@ -1,3 +1,25 @@
 const chai = require('chai');
-chai.use(chaiHttp);
+const { assert } = require('chai');
 
+
+//remove strings containing y or Y 
+
+describe('why', () => {
+
+    const numOrigin = [2, 3, 9, 12, 8, 5, 7];
+
+    it.skip('returns only the odd numbers in the array', () => {
+
+    });
+
+    it.skip('squares each odd number', () => {
+
+    });
+
+    it.skip('returns the squared values greater than 24', () => {
+
+    });
+});
+// add remaining strings to statement "is it..." 
+
+//return completed phrases
