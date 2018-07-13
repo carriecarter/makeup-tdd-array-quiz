193b7002fb08c3d0c560c9de23a0cc34fac0d419
carriecarter <carrie@carrie-carter.com>
2018-07-13 11:08:33 -0700

    out of time

--- test/why.test.js
+++ test/why.test.js
@@ -5,6 +5,8 @@ const { assert } = require('chai');
 describe('why', () => {
 
     const words = ['sky', 'hot', 'Yeti', 'green'];
+    const phrase = ['is it'];
+    const result = [];
 
     it.skip('returns only strings without a y or Y', () => {
         words.indexOf('Y') !== -1;
